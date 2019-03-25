55461d26d63c816e3c932a44befbe9e8ac45e83d
cruzayye <johnnyc010392@gmail.com>
2019-03-25 10:27:47 -0700

    all inputs change state

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Form.css';
 
-export default function Form({ onChange }) {
-  return (
-    <>
-    <section>
-      <h2>Create Artist Profile</h2>
-      <form className={styles.createForm}>
-        <input type='text' name='artistName' onChange={onChange} placeholder='Artist Name/Band Name' />
-        <input type='text' name='Email' onChange={onChange} placeholder='email' />
-        <input type='text' name='Profile Pic' onChange={onChange} placeholder='Profile Pic' />
-        <input type='text' name='Twitter' onChange={onChange} placeholder='Twitter' />
-        <input type='text' name='Facebook' onChange={onChange} placeholder='Facebook' />
-        <input type='text' name='Band Camp' onChange={onChange} placeholder='Band Camp' />
-        <textarea> </textarea>
-        <select name='genre'>
-          <option value="Indie">Indie</option>
-          <option value="Rock">Rock</option>
-          <option value="Hip-hop/Rap">Hip-hop/Rap</option>
-          <option value="Country">Country</option>
-        </select>  
-      </form>
-      <button>Submit</button> 
-    </section>
-    </>
-  );
+class Form extends PureComponent{
+  state = {
+    artistName: '',
+    email: '',
+    profilePic: '',
+    twitter: '',
+    website: '',
+    facebook: '',
+    bandCamp: '',
+
+
+
+  }
+
+  static propTypes = {
+    handleChange: PropTypes.func
+  }
+
+  handleChange = ({ target }) => {
+    target.preventDefault;
+    this.setState({ [target.name]: target.value });
+  }
+
+
+  render() {
+    return (
+      <>
+      <section>
+        <h2>Create Artist Profile</h2>
+        <form className={styles.createForm}>
+          <input type='text' name='artistName' text='artistName' onChange={this.handleChange} placeholder='Artist Name/Band Name' />
+          <input type='text' name='email' onChange={this.handleChange} placeholder='Email' />
+          <input type='text' name='profilePic' onChange={this.handleChange} placeholder='Profile Pic' />
+          <input type='text' name='twitter' onChange={this.handleChange} placeholder='Twitter' />
+          <input type='text' name='website' onChange={this.handleChange} placeholder='Website' />
+          <input type='text' name='facebook' onChange={this.handleChange} placeholder='Facebook' />
+          <input type='text' name='bandCamp' onChange={this.handleChange} placeholder='Band Camp' />
+          <select name='genre'>
+            <option value="indie">Indie</option>
+            <option value="rock">Rock</option>
+            <option value="hip-hop/rap">Hip-hop/Rap</option>
+            <option value="country">Country</option>
+          </select>  
+          <textarea name="description" placeholder="Describe yourself/band..."></textarea>
+        </form>
+        <button>Submit</button> 
+      </section>
+      </>
+    );
+  }
 
 }
 
-Form.propTypes = {
-  onChange: PropTypes.func
-};
+export default Form;
+
+
