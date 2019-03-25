a016fa16324d75d2f066410ecf83ccb00ff63073
Ivan <i.poblete915@gmail.com>
2019-03-25 10:02:12 -0700

    added hover effect to images

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ArtistList from '../artists/ArtistList';
-import Artist from '../artists/Artist';
+// import Artist from '../artists/Artist';
 
 export default function App() {
   return (
     <>
       <ArtistList />
-      <Artist />
+      {/* <Artist /> */}
     </>
   );
 }
--- src/components/artists/ArtistList.js
+++ src/components/artists/ArtistList.js
@@ -12,28 +12,40 @@ function ArtistList() {
       <h1>Artist List</h1>
       <ul className={styles.artistList}>
         <li>
-          <img src="https://via.placeholder.com/150" />
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           Artist1
         </li>
         <li>
-          <img src="https://via.placeholder.com/150" />
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           Artist2
         </li>
         <li>
-          <img src="https://via.placeholder.com/150" />
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           Artist3
         </li>
         <li>
-          <img src="https://via.placeholder.com/150" />
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           Artist4
         </li>
         <li>
-          <img src="https://via.placeholder.com/150" />
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           Artist5
         </li>
         <li>
-          <img src="https://via.placeholder.com/150" />
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           Artist6
+        </li>
+        <li>
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          Artist7
+        </li>
+        <li>
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          Artist8
+        </li>
+        <li>
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+          Artist9
         </li>
       </ul>
     </>
