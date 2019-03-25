618bdb62e6ef547d26ff679a446af0fae59ef530
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-25 09:43:45 -0700

    positioning styles

--- src/components/artist-detail/ArtistDetail.js
+++ src/components/artist-detail/ArtistDetail.js
@@ -1,9 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import styles from './ArtistDetail.css';
 
-export default class ArtistDetail extends PureComponent {
-  render() {
-    return (
+export default function ArtistDetail() {
+  return (
       <>
         <button className={styles.backButton}>←</button>
           <h3 className={styles.name}>Nirvana</h3>
@@ -19,22 +18,25 @@ export default class ArtistDetail extends PureComponent {
             <span>
               <p className={styles.description}>Nirvana was an American rock band formed in Aberdeen, Washington, in 1987. It was founded by lead singer and guitarist Kurt Cobain and bassist Krist Novoselic. Nirvana went through a succession of drummers, the longest-lasting and best-known being Dave Grohl, who joined in 1990.</p>
             </span>
-            <span>
-              <h4 className={styles.genreTitle}>Genre</h4>
-              <ul>
-                <li>Alternative Rock</li>
-                <li>Grunge</li>
-              </ul>
-            </span>
-            <div>
-              <h4>&#34;Smells Like Teen Spirit&#34;</h4>
-              <audio controls>
-                <source src='src/assets/song.mp3' type='audio/mpeg'/>
-              </audio>
+            <div className={styles.genreSongContainer}>
+              <div>
+                <h4 className={styles.genreTitle}>Genre</h4>
+                <ul>
+                  <div className={styles.genres}>
+                    <li>Alternative Rock</li>
+                    <li>Grunge</li>
+                  </div>
+                </ul>
+              </div>
+              <div className={styles.song}>
+                <h4 className={styles.songTitle}>&#34;Smells Like Teen Spirit&#34;</h4>
+                <audio controls>
+                  <source src='src/assets/song.mp3' type='audio/mpeg'/>
+                </audio>
+              </div>
             </div>
           </div>
         </main>
       </>
-    );
-  }
+  );
 }
