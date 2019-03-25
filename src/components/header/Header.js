9659fa442f3a21680331ca5b228c7567985c0856
Ivan <i.poblete915@gmail.com>
2019-03-25 14:13:13 -0700

    added background image to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Header.css';
 
 export default function Header() {
   return (
-    <header>
-      <h1>PDX Pop Now Search</h1>
+    <header className={styles.header}>
+      <img className={styles.background} src="src/assets/abstract-music-rock-bw.jpg" />
     </header>
   );
 }
