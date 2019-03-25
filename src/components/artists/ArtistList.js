1c5806dc9422efa8bab73b69559c4efb5ce8073b
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-25 15:05:34 -0700

    artistlist is now functional component

--- src/components/artists/ArtistList.js
+++ src/components/artists/ArtistList.js
@@ -1,25 +1,26 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import styles from './ArtistList.css';
 import PropTypes from 'prop-types';
 
-export default class ArtistList extends PureComponent {
-  static propTypes = {
-    artists: PropTypes.array.isRequired
-  }
-  render() {
-    const { artists } = this.props;
-    const artistList = artists.map((artist, i) => { 
-      return <li key={i}>
-        <img src={artist.url}/>
-        { artist.name }
-      </li>;
-    });
-    return (
-      <section className={styles.artistListSection}>
-        <ul className={styles.artistList}>
-          { artistList }
-        </ul>
-      </section>
-    );
-  }
+function ArtistList(props){
+  const { artists } = props;
+  const artistList = artists.map((artist, i) => { 
+    return <li key={i}>
+      <img src={artist.url}/>
+      { artist.name }
+    </li>;
+  });
+  return (
+    <section className={styles.artistListSection}>
+      <ul className={styles.artistList}>
+        { artistList }
+      </ul>
+    </section>
+  );
 }
+
+
+ArtistList.propTypes = {
+  artists: PropTypes.array.isRequired
+};
+export default ArtistList;
