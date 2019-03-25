7e7c3ac277a568cee0395de8ee2952c88fea5d19
Ivan <i.poblete915@gmail.com>
2019-03-25 13:38:22 -0700

    Header created and rendering on browser

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ArtistList from '../artists/ArtistList';
+import Header from '../header/Header';
 
 export default function App() {
   return (
     <>
+    <Header />
       <ArtistList />
     <h1>Hello world</h1>
     </>
