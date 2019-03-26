d632251204e3051a988cb42a6d7502f535577629
Sophie Graham <sophiergraham1@gmail.com>
2019-03-26 16:43:30 -0700

    changes spacing

--- src/containers/auth/withSession.js
+++ src/containers/auth/withSession.js
@@ -4,21 +4,21 @@ import { login } from '../../services/auth';
 import PropTypes from 'prop-types';
 
 export const withSession = Component => {
-class WithSession extends PureComponent {
-  static propTypes = {
-    token: PropTypes.func
-  }
-  componentDidMount() {
-    if(!this.props.token) {
-      login();
+  class WithSession extends PureComponent {
+    static propTypes = {
+      token: PropTypes.func
+    }
+    componentDidMount() {
+      if(!this.props.token) {
+        login();
+      }
+    }
+
+    render() {
+      if(!this.props.token) return <h1>LOADING</h1>;
+      return <Component {...this.props} />;
     }
   }
-
-  render() {
-    if(!this.props.token) return <h1>LOADING</h1>;
-    return <Component {...this.props} />;
-  }
-}
 
   const mapStateToProps = state => ({
     token: state.auth.token
