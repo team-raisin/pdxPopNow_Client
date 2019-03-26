a412f1975acf182cbfb2a6ca77bd9da43e29782a
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-26 13:21:35 -0700

    tell travis to chill

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { getRoutes } from '../../routes/routes';
+// import { BrowserRouter as Router, Switch } from 'react-router-dom';
+// import { getRoutes } from '../../routes/routes';
 import AllArtists from '../../containers/AllArtists';
 
 export default function App() {
