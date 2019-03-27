65fd3c7c37bcd574fa2e2f790e043269480f2929
Sophie Graham <sophiergraham1@gmail.com>
2019-03-27 11:38:58 -0700

    added with session to routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import AllArtists from '../containers/AllArtists';
 import CreateArtist from '../containers/CreateArtist';
+import Callback from '../containers/auth/Callback';
+import { withSession } from '../containers/auth/withSession';
 
 export const ROUTES = {
   ArtistForm: {
     path: '/artistform',
-    Component: CreateArtist,
+    Component: withSession(CreateArtist),
     linkTo: () => '/artistform'
   },
+
   Artist: {
     path: '/artists',
     Component: AllArtists,
     linkTo: () => '/artists'
+  },
+  
+  CALLBACK: {
+    path: '/callback',
+    Component: Callback,
+    linkTo: () => '/callback'
   }
 };
 
