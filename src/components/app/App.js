2a9dbda86caca5e6b6351765f06b787c3f22cd22
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-27 09:34:30 -0700

    clicking on artist takes you to generic detail

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import { getRoutes } from '../../routes/routes';
 import AllArtists from '../../containers/AllArtists';
+import ArtistDetail from '../../components/artist-detail/ArtistDetail';
 // import { login } from '../../services/auth';
 import Callback from '../../containers/auth/Callback';
 import { withSession } from '../../containers/auth/withSession';
@@ -11,6 +12,7 @@ export default function App() {
     <Router>
       <Switch>
         <Route path="/callback" component={Callback} />
+        <Route path="/:id" component={ArtistDetail} />
         <Route path="/" component={AllArtists} />
       </Switch>
     </Router>
--- src/components/artists/ArtistList.js
+++ src/components/artists/ArtistList.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import styles from './ArtistList.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function ArtistList(props){
   const { artists, handleClick } = props;
   const artistList = artists.map((artist, i) => { 
-    return <li key={i} onClick={handleClick}>
-      <img src={artist.img}/>
-      { artist.artistName }
-    </li>;
+    return <Link to={`/artist/${artist._id}`} key={i}><li onClick={handleClick}><img src={artist.img}/>{ artist.artistName }</li></Link>;
   });
+  console.log(artists[0]);
   return (
     <section className={styles.artistListSection}>
       <ul className={styles.artistList}>
