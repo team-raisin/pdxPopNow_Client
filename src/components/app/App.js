c0340f4d4c223529f94c425be551069d96a88a6a
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-26 17:47:19 -0700

    travis fix

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import { getRoutes } from '../../routes/routes';
-import AllArtists from '../../containers/AllArtists';
+// import AllArtists from '../../containers/AllArtists';
 // import { login } from '../../services/auth';
-import Callback from '../../containers/auth/Callback';
+// import Callback from '../../containers/auth/Callback';
 import CreateArtist from '../../containers/CreateArtist';
 
 export default function App() {
