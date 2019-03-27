e66f891032b5e685c4532850f3beb7532067c3d7
cruzayye <johnnyc010392@gmail.com>
2019-03-27 15:19:29 -0700

    capitalized select options

--- src/components/artist-search/ArtistSearch.js
+++ src/components/artist-search/ArtistSearch.js
@@ -30,9 +30,31 @@ export default class ArtistSearch extends PureComponent {
     return (
       <form className={styles.search} onSubmit={this.handleSubmit}>
         <select name='search' onChange={this.handleChange} value={this.state.search}>
-          <option value="Acoustic">Acoustic</option>
-          <option value="Blues">Blues</option>
-          <option value="Country">Country</option>
+          <option name='genre' value="Genre">Genre</option>
+          <option name='genre' value="Acoustic">Acoustic</option>
+          <option name='genre' value="Blues">Blues</option>
+          <option name='genre' value="Country">Country</option>
+          <option name='genre' value="Edm">EDM</option>
+          <option name='genre' value="Electro">Electro</option>
+          <option name='genre' value="Experimental">Experimental</option>
+          <option name='genre' value="Folk">Folk</option>
+          <option name='genre' value="Funk">Funk</option>
+          <option name='genre' value="Garage">Garage</option>
+          <option name='genre' value="Hip-Hop">Hip-hop</option>
+          <option name='genre' value="Indie">Indie</option>
+          <option name='genre' value="Jazz">Jazz</option>
+          <option name='genre' value="Metal">Metal</option>
+          <option name='genre' value="Pop">Pop</option>
+          <option name='genre' value="Psych">Psych</option>
+          <option name='genre' value="Punk">Punk</option>
+          <option name='genre' value="r&amp;b">R&amp;B</option>
+          <option name='genre' value="Rap">Rap</option>
+          <option name='genre' value="Reggae">Reggae</option>
+          <option name='genre' value="Regular">Regular</option>
+          <option name='genre' value="Rock">Rock</option>
+          <option name='genre' value="Soul">Soul</option>
+          <option name='genre' value="Techno">Techno</option>
+          <option name='genre' value="World">World</option>
         </select>
         <input className={styles.searchInput} onChange={this.handleChange} type="text" name="search" placeholder="Search artist, band, genre" />
         <button>Search</button>
