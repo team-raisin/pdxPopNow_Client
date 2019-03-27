eed8e2d774f65ced89e33303a4c1361b14923b80
Johnny Cruz <40680065+cruzayye@users.noreply.github.com>
2019-03-27 15:30:44 -0700

    Search2 (#23)
    * artist search filters by whatever gets typed in about to move onto genre filtering
    
    * searchBar gets band by genre need to implement actual dropdownmenu
    
    * selector works on change
    
    * capitalized select options

--- src/selectors/artists.js
+++ src/selectors/artists.js
@@ -1,6 +1,27 @@
 export const getArtists = state => {
   return state.artists.artists;
 };
+
+export const getSearchArtist = state => {
+  return state.artists.artistSearch;
+};
+
+export const getFilteredArtists = state => {
+  const searchTerm = getSearchArtist(state);
+  return getArtists(state).filter(artist => {
+    return artist.genre.includes(searchTerm) || artist.artistName.includes(searchTerm);
+  });
+};
+// export const getFilteredArtists = state => {
+//   const searchTerm = getSearchArtist(state);
+//   return getArtists(state).filter(artist => {
+//     return artist.artistName.includes(searchTerm) || artist.genre.includes(searchTerm);
+//   });
+// };
+
+
+
+//Johnny needs to check if we still need this which we most likely dont
 export const getEmail = state => {
   return state.artists.email;
 };
