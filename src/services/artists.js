feb56e39313fdd4e45d53b86973a81e74cecbfa0
Sophie Graham <sophiergraham1@gmail.com>
2019-03-27 17:12:19 -0700

    aded put into selectors

--- src/services/artists.js
+++ src/services/artists.js
@@ -1,4 +1,5 @@
-import { get, post } from './request';
+import { get, post, put } from './request';
 
 export const getArtists = () => get('/artist');
 export const postArtist = artist => post('/artist', artist);
+export const putArtist = (id, artist) => put(`/artist/${id}`, artist);
