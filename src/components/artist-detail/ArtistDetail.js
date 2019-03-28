ad1c74b023ddfb406d498db5936c9b9c614c98a1
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-28 14:55:51 -0700

    page fades in on load

--- src/components/artist-detail/ArtistDetail.js
+++ src/components/artist-detail/ArtistDetail.js
@@ -10,7 +10,8 @@ import {
   FaSoundcloud,
   FaVimeo,
   FaBandcamp,
-  FaDesktop
+  FaDesktop,
+  FaArrowCircleLeft
 } from 'react-icons/fa';
 
 
@@ -18,33 +19,33 @@ export default function ArtistDetail({ artist }) {
   console.log(artist);
   const link = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/${artist.soundcloudId}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`;
   return (
-      <>
-        <Link className={styles.backButton} to='/'>←</Link>
-        <h3 className={styles.name}>{artist.artistName}</h3>
-        <main className={styles.main}>
-          <div className={styles.pictureContainer}>
-            <img className={styles.picture} src={artist.img}/>
-          </div>
-          <div className={styles.rightColumn}>
-            <p className={styles.description}>{artist.description}</p>
-          </div>
-        </main>
-        <div className={styles.songSection}>
-          {artist.soundcloudId && <iframe width="50%" height="600" scrolling="no" frameBorder="no" allow="autoplay" src={link}></iframe>}
+    <div className={styles.all}>
+      <Link to='/'><FaArrowCircleLeft className={styles.backButton}/></Link>
+      <h3 className={styles.name}>{artist.artistName}</h3>
+      <main className={styles.main}>
+        <div className={styles.pictureContainer}>
+          <img className={styles.picture} src={artist.img}/>
         </div>
-        <div className={styles.socialSection}>
-          <div className={styles.socialMediaContainer}>
-            {artist.facebook && <a href={artist.facebook}><FaFacebook className={styles.facebook}/></a>}
-            {artist.instagram && <a href={artist.instagram}><FaInstagram className={styles.instagram}/></a>}
-            {artist.twitter && <a href={artist.twitter}><FaTwitter className={styles.twitter}/></a>}
-            {artist.youtube && <a href={artist.youtube}><FaYoutube className={styles.youtube}/></a>}
-            {artist.soundcloud && <a href={artist.soundcloud}><FaSoundcloud className={styles.soundcloud}/></a>}
-            {artist.vimeo && <a href={artist.vimeo}><FaVimeo className={styles.vimeo}/></a>}
-            {artist.bandcamp && <a href={artist.bandcamp}><FaBandcamp className={styles.bandcamp}/></a>}
-            {artist.website && <a href={artist.website}><FaDesktop className={styles.website}/></a>}
-          </div>
+        <div className={styles.rightColumn}>
+          <p className={styles.description}>{artist.description}</p>
         </div>
-      </>
+      </main>
+      <div className={styles.songSection}>
+        {artist.soundcloudId && <iframe width="50%" height="600" scrolling="no" frameBorder="no" allow="autoplay" src={link}></iframe>}
+      </div>
+      <div className={styles.socialSection}>
+        <div className={styles.socialMediaContainer}>
+          {artist.facebook && <a href={artist.facebook}><FaFacebook className={styles.facebook}/></a>}
+          {artist.instagram && <a href={artist.instagram}><FaInstagram className={styles.instagram}/></a>}
+          {artist.twitter && <a href={artist.twitter}><FaTwitter className={styles.twitter}/></a>}
+          {artist.youtube && <a href={artist.youtube}><FaYoutube className={styles.youtube}/></a>}
+          {artist.soundcloud && <a href={artist.soundcloud}><FaSoundcloud className={styles.soundcloud}/></a>}
+          {artist.vimeo && <a href={artist.vimeo}><FaVimeo className={styles.vimeo}/></a>}
+          {artist.bandcamp && <a href={artist.bandcamp}><FaBandcamp className={styles.bandcamp}/></a>}
+          {artist.website && <a href={artist.website}><FaDesktop className={styles.website}/></a>}
+        </div>
+      </div>
+    </div>
   );
 }
 
