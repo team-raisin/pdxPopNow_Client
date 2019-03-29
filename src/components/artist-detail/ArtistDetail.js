e85d315b4aebe722ae4d420ae3dde9f64aa292ae
pickuppatterns <pickup.patterns@gmail.com>
2019-03-28 18:36:41 -0700

    updates tw link

--- src/components/artist-detail/ArtistDetail.js
+++ src/components/artist-detail/ArtistDetail.js
@@ -39,7 +39,7 @@ export default function ArtistDetail({ artist }) {
         <div className={styles.socialMediaContainer}>
           {artist.facebook && <a href={artist.facebook}target='_blank' rel="noopener noreferrer"  ><FaFacebook className={styles.facebook}/></a>}
           {artist.instagram && <a href={artist.instagram} target='_blank' rel="noopener noreferrer"  ><FaInstagram className={styles.instagram}/></a>}
-          {artist.twitter && <a href={artist.twitter}><FaTwitter className={styles.twitter}/></a>}
+          {artist.twitter && <a href={artist.twitter} target='_blank' rel="noopener noreferrer"  ><FaTwitter className={styles.twitter}/></a>}
           {artist.youtube && <a href={artist.youtube}><FaYoutube className={styles.youtube}/></a>}
           {artist.soundcloudId && <a href={artist.soundcloud}><FaSoundcloud className={styles.soundcloud}/></a>}
           {artist.vimeo && <a href={artist.vimeo}><FaVimeo className={styles.vimeo}/></a>}
