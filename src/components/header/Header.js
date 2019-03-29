de634939faea29d5a7f2b57f0a894bcce1d60ac6
cruzayye <johnnyc010392@gmail.com>
2019-03-28 17:12:04 -0700

    made form component we will need to add this component into each page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from './Nav';
 import styles from './Header.css';
 
 export default function Header() {
@@ -6,16 +7,7 @@ export default function Header() {
     <header className={styles.header}>
       <h2>PDX POP <br/>NOW</h2>
       {/* <img className={styles.background} src="src/assets/abstract-music-rock-bw.jpg" /> */}
-      <aside>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Sign in</li>
-            <li>create Artist</li>
-          </ul>
-        </nav>
-      </aside>
+      <Nav />
     </header>
   );
 }
